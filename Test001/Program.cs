f4b637b95ef44c1d5f2446e26e0f42d1ea39c335
HPUMeran <h.umilietti@alice.it>
2020-04-27 10:05:19 +0200

    Update Program.cs

--- Test001/Program.cs
+++ Test001/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//** Umrechnung zwischen verschiedenen Zahlensystemen
+//** Umrechnung zwischen verschiedenen Zahlensystemen (2)
 namespace Test001
 {
     class Program
